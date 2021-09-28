319acc6e1a5b72875f94b074c1548a57bca03ac3
Лилия <80756964+peachysangria@users.noreply.github.com>
2021-09-28 21:11:39 +0300

    done!

--- vknews/vknews/DetailedPostViewController.swift
+++ vknews/vknews/DetailedPostViewController.swift
@@ -9,23 +9,37 @@ import UIKit
 
 class DetailedPostViewController: UIViewController {
 
+    @IBOutlet weak var groupImageView: UIImageView!
+    
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
+    @IBOutlet weak var publishTimeLabel: UILabel!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    @IBOutlet weak var image3: UIImageView!
+    
+    @IBOutlet weak var image4: UIImageView!
+    
     var post: SinglePostCellData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        groupImageView.image = post?.groupImage
+        groupNameLabel.text = post?.groupName
+        publishTimeLabel.text = post?.publishingTime
+        descriptionTextView.text = post?.description
+        image1.image = post?.pinnedPics[0]
+        if post?.pinnedPics.count ?? 0 > 1 {
+            image2.image = post?.pinnedPics[1]
+        }
+        if post?.pinnedPics.count ?? 0 > 2 {
+            image3.image = post?.pinnedPics[2]
+            image4.image = post?.pinnedPics[3]
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- vknews/vknews/PostTableViewCell.swift
+++ vknews/vknews/PostTableViewCell.swift
@@ -31,15 +31,15 @@ class PostTableViewCell: UITableViewCell {
     }
 
     func SetData(post: SinglePostCellData) {
-        groupNameLabel.text = post.groupName
-        descriptionTextView.text = post.description
         groupImageView.image = post.groupImage
+        groupNameLabel.text = post.groupName
         pubTimeLabel.text = post.publishingTime
+        descriptionTextView.text = post.description
         postPicImageView.image = post.pinnedPics[0]
-        if post.pinnedPics.count == 2 {
+        if post.pinnedPics.count > 1 {
             postPicImageView2.image = post.pinnedPics[1]
         }
-        if post.pinnedPics.count == 4 {
+        if post.pinnedPics.count > 2 {
             postPicImageView3.image = post.pinnedPics[2]
             postPicImageView4.image = post.pinnedPics[3]
         }
